086a630fa80b53175238173704a8d4ef0f179793
Audrey Baniant <audrey.baniant@gmail.com>
2023-08-18 18:00:59 -0400

    Functionning app, needs CSS, reviews, API

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 
+  has_one_attached :photo
+
   validates :name, uniqueness: true
   validates :name, presence: true
 end
